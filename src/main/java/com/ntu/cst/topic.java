68a70d82fbca1d3b755f5cea68149e395fcce40c
mbh1210554992 <mbh1210554992@qq.com>
2020-05-01 22:13:00 +0800

    update

--- src/main/java/com/ntu/cst/topic.java
+++
@@ -1,8 +0,0 @@
-package com.ntu.cst;
-
-public class topic 
-{
-	public static String TOPIC_SYNC_CENTER="/sync/center";
-	public static String TOPIC_SYNC_ALL="/sync/all";
-	public static String TOPIC_SYNC_NODE="/sync/";
-}
--- src/main/java/com/ntu/datasync/common/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.ntu.datasync.common;
-
-/**
- * @Author: baihua
- * @Date: Created in 11/12/2019 2:52 PM
- */
-public class Constants {
-
-}
